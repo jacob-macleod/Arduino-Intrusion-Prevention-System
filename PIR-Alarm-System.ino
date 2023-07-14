85b1c0c6162773343566beeb424e73bd359fe69c
jacob-macleod <jacob.e.macleod@gmail.com>
2023-07-14 16:29:11 +0100

    Added servo motor

--- PIR-Alarm-System.ino
+++ PIR-Alarm-System.ino
@@ -1,14 +1,19 @@
+#include <Servo.h>
+
 int led = 13;                // the pin the LED is atteched to
 int sensor = 2;              // the pin the PIR sensor is atteched to
 int state = LOW;             // LOW = no motion, HIGH = motion
 int val = 0;                 // Stores sensor status
 int buzzerPin = 12;          // The pin the buzzer is attached to
+Servo eyePivot;              // The servo controlling the eyes
+int pos = 0;                 // The position of eyePivot
 
 void setup() {
   // Initialise IO
   pinMode(led, OUTPUT);
   pinMode(buzzerPin, OUTPUT);
   pinMode(sensor, INPUT); 
+  eyePivot.attach(11);
 
   Serial.begin(9600);
 }
@@ -28,14 +33,14 @@ void deactivateAlarm() {
   digitalWrite(led, LOW);
 }
 
-void loop() {
+void checkPIRSensor () {
   // Read sensor status
   val = digitalRead(sensor);
 
   // If motion is detected
   if (val == HIGH) {           
     activateAlarm();
-    delay(100);                
+    //delay(100);                
     
     // If the motion has been detected for the first time
     if (state == LOW) {
@@ -45,11 +50,27 @@ void loop() {
   } 
   else {
       deactivateAlarm();
-      delay(200);             
+      //delay(200);             
       
       if (state == HIGH){
         Serial.println("Motion stopped!");
         state = LOW;       
     }
   }
+}
+
+void loop() {
+  // Spin the servo
+  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+    eyePivot.write(pos);
+    checkPIRSensor();            
+    delay(15);                       
+  }
+  for (pos = 180; pos >= 0; pos -= 1) {
+    eyePivot.write(pos);
+    checkPIRSensor();         
+    delay(15);                       
+  }
+
+  /**/
 }
